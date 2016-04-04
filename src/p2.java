b5d3937fa848554c7740073aa53043e77103d9f4
bufy94 <indreiu.ovidiu@gmail.com>
2016-04-05 01:49:52 +0300

    tema2

---
+++ src/p2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * Created by Indreiu on 05-Apr-16.
+ */
+public class p2 {
+    public static void main(String[] a) {
+
+        int n = 20;
+        int s=0;
+        for(int i=1;i<n+1;i++)
+            s+=i;
+
+    }
+}
